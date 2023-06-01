77035610fd940cd437b81a88de8ec1f9a26c0d1e
Anthony <anth06ny@gmail.com>
2023-06-01 10:19:04 +0200

    Correction Manip IHM

--- app/src/main/java/com/amonteiro/a2023_05_equans/MainActivity.kt
+++ app/src/main/java/com/amonteiro/a2023_05_equans/MainActivity.kt
@@ -3,24 +3,37 @@ package com.amonteiro.a2023_05_equans
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import android.widget.Button
 import com.amonteiro.a2023_05_equans.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    //Créer l'IHM
-    val binding by lazy {  ActivityMainBinding.inflate(layoutInflater)}
+    //Créer une instance de l'ihm
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    //Ecran qui va être créé
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //Affichage
+        //Affichage IHM
         setContentView(binding.root)
 
+        //Callback (objet anonyme) clic sur le bouton
         binding.btValidate.setOnClickListener {
-            println("coucou")
+            binding.et.setText("Clic sur valider")
         }
 
+        //callback par interface
+        binding.btCancel.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        //Identification du bouton cliqué
+        if(v === binding.btCancel) {
+            binding.et.setText("Clic sur annuler")
+        }
     }
 
 }
